b5709fdf6efeadb4a5d710b33eb102ea7c1020ce
nks-19 <nehakumarisingh1904@gmail.com>
2024-04-28 11:52:20 +0530

    Adding a redirects file

--- src/App.js
+++ src/App.js
@@ -51,9 +51,6 @@ function App() {
                     <Nav activeKey={window.location.pathname}>
                         {isAuthenticated ? (
                             <>
-                              <LinkContainer to="/settings">
-                                  <Nav.Link>Settings</Nav.Link>
-                              </LinkContainer>
                               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
                             </>
                         ) : (
